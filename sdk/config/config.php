36a643471507afb13c11e3ec1dd3fa0444c8e800
Thayller Vilela Cintra <thayllervilelacintra@yahoo.com.br>
2019-04-29 15:55:32 -0300

    Add files via upload

---
+++ sdk/config/config.php
@@ -0,0 +1,5 @@
+<?php
+
+define("APP_ID",4597458944043665);
+define("SECRET_KEY","OOgpZFDXaXNRZkU6BtTjGndDxIGqHjRz");
+define("URL_CALLBACK","https://lojateste.pxw.com.br/loja/mercadoLivre/mercadoLivre.php");
