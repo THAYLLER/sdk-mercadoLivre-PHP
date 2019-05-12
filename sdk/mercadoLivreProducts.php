6e076b497a938ac165fbace4e676067e02a91491
Thayller Vilela Cintra <thayllervilelacintra@yahoo.com.br>
2019-05-11 23:49:40 -0300

    Add files via upload

--- sdk/mercadoLivreProducts.php
+++ sdk/mercadoLivreProducts.php
@@ -5,17 +5,15 @@ class mercadoLivreProducts extends mercadoLivreActions{
     private $title;
     private $category_id;
     private $price;
-    private $official_store_id;
     private $currency_id;
     private $available_quantity;
     private $buying_mode;
     private $listing_type_id;
-    private $condition;
     private $description;
     private $video_id;
-    private $warranty;
+    private $attributes;
+    private $sale_terms;
     private $pictures;
-    private $tags;
     private $token;
     
     public function __set($atrib, $value) {
@@ -39,11 +37,10 @@ class mercadoLivreProducts extends mercadoLivreActions{
             "available_quantity" => $this->available_quantity,
             "buying_mode" => $this->buying_mode,
             "listing_type_id" => $this->listing_type_id,
-            "condition" => $this->condition,
             "description" => $this->description,
             "video_id" => $this->video_id,
-            "tags" => $this->tags,
-            "warranty" => $this->warranty,
+            "attributes" => $this->attributes,
+            "sale_terms" => $this->sale_terms,
             "pictures" => $this->pictures
         );
     }
